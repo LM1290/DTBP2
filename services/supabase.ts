bc2190c751d6a25650e1c9efe0d7f903bdddaa04
LM1290 <dunkingguy2333@gmail.com>
2026-01-13 21:03:04 -0500

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Replace these with your actual Supabase Project URL and Anon Key
-const supabaseUrl = 'https://your-project-id.supabase.co';
-const supabaseAnonKey = 'your-public-anon-key';
+const supabaseUrl = 'https://zhvamtwqhwkhieumpfjs.supabase.co';
+const supabaseAnonKey = 'sb_publishable_sGdjJ3oB4S31G52RMqwhKw_J9WnR0Q-';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
